dc0a2b9131c2cdd5333f194950c3af0ea28ccdf4
Agust <agustfricke@protonmail.com>
2023-08-25 17:10:52 -0300

    fix delete url

---
+++ handlers/task.go
@@ -0,0 +1,108 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/agustfricke/go-htmx-crud/database"
+	"github.com/agustfricke/go-htmx-crud/models"
+)
+
+
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+        db := database.DB 
+	    var tasks []models.Task
+	    db.Find(&tasks)
+
+	    tmpl := template.Must(template.ParseFiles("templates/index.html"))
+
+	    err := tmpl.Execute(w, tasks)
+	    if err != nil {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	    }
+	}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+		name := r.PostFormValue("name")
+        
+        var task models.Task 
+        if name != "" {
+            db := database.DB
+            task = models.Task{Name: name} 
+            db.Create(&task)
+        }
+	    data := struct {Task models.Task}{Task: task,}
+
+	    tmpl := template.Must(template.ParseFiles("templates/item.html"))
+	    err :=  tmpl.Execute(w, data)
+	    if err != nil {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	    }
+
+	}
+
+func DeleteTask(w http.ResponseWriter, r *http.Request) {
+    time.Sleep(1 * time.Second)
+    
+    parts := strings.Split(r.URL.Path, "/")
+    if len(parts) != 3 {
+        http.Error(w, "Invalid URL", http.StatusBadRequest)
+        return
+    }
+    ID := parts[2]
+    
+    db := database.DB
+
+    var task models.Task
+    db.First(&task, ID)
+    db.Delete(&task)
+}
+
+func FormEditTask(w http.ResponseWriter, r *http.Request) {
+        name := r.URL.Query().Get("name")
+        ID := r.URL.Query().Get("ID")
+	    data := struct {ID string; Name string}{ID: ID, Name: name}
+
+	    tmpl := template.Must(template.ParseFiles("templates/edit.html"))
+	    err :=  tmpl.Execute(w, data)
+	    if err != nil {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		    return
+	    }
+    }
+    
+func EditTask(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(1 * time.Second)
+
+        name := r.PostFormValue("name")
+        ID := r.PostFormValue("ID") 
+        db := database.DB
+
+        var task models.Task
+        if err := db.First(&task, ID).Error; err != nil {
+            fmt.Printf("NOp")
+        }
+
+        task.Name = name 
+
+        if err := db.Save(&task).Error; err != nil {
+            fmt.Printf("Error al guardar la tarea: %s\n", err)
+        }
+
+	    data := struct {Task models.Task}{Task: task}
+
+	    tmpl := template.Must(template.ParseFiles("templates/item.html"))
+	    err :=  tmpl.Execute(w, data)
+	    if err != nil {
+		    http.Error(w, err.Error(), http.StatusInternalServerError)
+		    return
+	    }
+
+	}
+
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/agustfricke/go-htmx-crud/database"
-	"github.com/agustfricke/go-htmx-crud/models"
+	"github.com/agustfricke/go-htmx-crud/handlers"
 )
 
 
@@ -16,102 +14,12 @@ func main() {
 
     database.ConnectDB()
 
-	fmt.Println("Go app...")
+	http.HandleFunc("/add/", handlers.CreateTask)
+	http.HandleFunc("/delete/", handlers.DeleteTask)
+	http.HandleFunc("/edit/form/", handlers.FormEditTask)
+	http.HandleFunc("/put/", handlers.EditTask)
+    http.HandleFunc("/", handlers.GetTasks)
 
-	h1 := func(w http.ResponseWriter, r *http.Request) {
-        db := database.DB 
-	    var tasks []models.Task
-	    db.Find(&tasks)
-
-	    tmpl := template.Must(template.ParseFiles("index.html"))
-
-	    err := tmpl.Execute(w, tasks)
-	    if err != nil {
-		    http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	    }
-	}
-
-	h2 := func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(1 * time.Second)
-		name := r.PostFormValue("name")
-        
-        var task models.Task 
-        if name != "" {
-            db := database.DB
-            task = models.Task{Name: name} 
-            db.Create(&task)
-        }
-	    data := struct {Task models.Task}{Task: task,}
-
-	    tmpl := template.Must(template.ParseFiles("item.html"))
-	    err :=  tmpl.Execute(w, data)
-	    if err != nil {
-		    http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	    }
-
-	}
-
-	h3 := func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(1 * time.Second)
-        ID := r.URL.Query().Get("ID")
-	    db := database.DB
-
-	    var task models.Task
-	    db.First(&task, ID)
-	    db.Delete(&task)
-	}
-
-    h4 := func(w http.ResponseWriter, r *http.Request) {
-        name := r.URL.Query().Get("name")
-        ID := r.URL.Query().Get("ID")
-	    data := struct {ID string; Name string}{ID: ID, Name: name}
-
-	    tmpl := template.Must(template.ParseFiles("edit.html"))
-	    err :=  tmpl.Execute(w, data)
-	    if err != nil {
-		    http.Error(w, err.Error(), http.StatusInternalServerError)
-		    return
-	    }
-    }
-
-	h5 := func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(1 * time.Second)
-
-        name := r.PostFormValue("name")
-        ID := r.PostFormValue("ID") 
-        db := database.DB
-
-        var task models.Task
-        if err := db.First(&task, ID).Error; err != nil {
-            fmt.Printf("NOp")
-        }
-
-        task.Name = name 
-
-        if err := db.Save(&task).Error; err != nil {
-            fmt.Printf("Error al guardar la tarea: %s\n", err)
-        }
-
-	    data := struct {Task models.Task}{Task: task}
-
-	    tmpl := template.Must(template.ParseFiles("item.html"))
-	    err :=  tmpl.Execute(w, data)
-	    if err != nil {
-		    http.Error(w, err.Error(), http.StatusInternalServerError)
-		    return
-	    }
-
-	}
-
-
-	// define handlers
-	http.HandleFunc("/", h1)
-	http.HandleFunc("/add/", h2)
-	http.HandleFunc("/delete/", h3)
-	http.HandleFunc("/edit/form/", h4)
-	http.HandleFunc("/put/", h5)
-
+	fmt.Println("Runnning in port 8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
